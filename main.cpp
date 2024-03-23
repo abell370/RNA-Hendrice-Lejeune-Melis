95585291edf5bec692b3e950b8816ce9016a1833
abell370 <ituniverse.sandbox@gmail.com>
2024-03-23 10:05:46 +0100

    + ui add chart

--- main.cpp
+++ main.cpp
@@ -5,16 +5,18 @@
 #include <qlineseries.h>
 #include <qchartview.h>
 #include <qscatterseries.h>
+#include <qlayout.h>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
+
     vector<DataSet> data = {};
     MainController* mainController = new MainController(data);
     MainWindow w(mainController);
-    w.show();
+
 
     QLineSeries* identitySeries = new QLineSeries();
     for (int x = 0; x <= 10; ++x) {
@@ -22,25 +24,32 @@ int main(int argc, char* argv[])
     }
     identitySeries->setName("Identity Function");
 
-
     QScatterSeries* randomSeries = new QScatterSeries();
     for (int i = 0; i < 10; ++i) {
         QPoint point(i, rand() % 10); // Generate random points
         randomSeries->append(point);
     }
     randomSeries->setName("Random Points");
-    // Create a chart and add the series
+
     QChart* chart = new QChart();
     chart->addSeries(identitySeries);
     chart->addSeries(randomSeries);
     chart->setTitle("Simple Line Chart");
-    chart->createDefaultAxes(); // Create default axes
-
-    // Create a chart view and set the chart
+    chart->createDefaultAxes(); 
     QChartView* chartView = new QChartView(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
+    QWidget* chartWidget = w.findChild<QWidget*>("chart");
 
-    w.setCentralWidget(chartView);
+    if (chartWidget) {
+        QVBoxLayout* chartLayout = new QVBoxLayout();
+        chartLayout->addWidget(chartView);
+        chartWidget->setLayout(chartLayout);
+    }
+    else {
+        cerr << "Error: Could not find widget named 'chart'." << endl;
+        return 1;
+    }
+
     w.show();
     return a.exec();
 }
